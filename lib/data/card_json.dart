0f8453753cf3591902a790d8d319fbbfcbcc6d67
Fathan Radhiyan <fathanradhiyan@gmail.com>
2021-11-25 10:43:30 +0700

    finishing touch

--- lib/data/card_json.dart
+++ lib/data/card_json.dart
@@ -4,4 +4,5 @@ const List card_json = [
   {'img': 'assets/images/card3.png', 'value': 4},
   {'img': 'assets/images/card4.png', 'value': 8},
   {'img': 'assets/images/card5.png', 'value': 16},
+  {'img': 'assets/images/card6.png', 'value': 0},
 ];
